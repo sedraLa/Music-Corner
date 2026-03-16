a4db588a9f76460e50b003a3f320c3aa5621be12
sedraLa <mhamadlaila42@gmail.com>
2026-03-15 20:01:15 -0700

    cjange welcome page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,12 +32,20 @@
                 </label>
             </div>
 
+            <div class="block mt-4">
+                <label for="remember_me" class="flex items-center">
+                    <a class="ms-2 text-sm text-gray-600" href="{{route('register')}}">Don't have an email?</a>
+                </label>
+            </div>
+
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
+
+
 
                 <x-button class="ms-4">
                     {{ __('Log in') }}
